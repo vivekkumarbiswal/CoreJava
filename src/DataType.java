6af795430176b8cbe2d18162d726019eba24364a
vivekkumarbiswal <vivekkumarbiswal@gmail.com>
2026-04-10 14:38:33 +0530

    DataTypes

---
+++ src/DataType.java
@@ -0,0 +1,22 @@
+public class DataType {
+    public static void main(String[] args) {
+
+        int num = 10;
+        byte bnum = 127;
+        short sh = 120;
+        long l = 43433;
+        char ch = 'a';
+        String str = "Vivek";
+        double dnum = 3.4;
+        float fnum = 5.6f;
+
+        System.out.println(num);
+        System.out.println(bnum);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(ch);
+        System.out.println(str);
+        System.out.println(dnum);
+        System.out.println(fnum);
+    }
+}
